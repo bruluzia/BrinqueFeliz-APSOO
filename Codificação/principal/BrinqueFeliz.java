ee62d6673abc793caa906aec3a92d55a96b9835f
brunaluzia <brunnnaluzia@gmail.com>
2018-12-02 14:41:28 -0300

    Adicionei classe principal do projeto com comentários

---
+++ Codificação/principal/BrinqueFeliz.java
@@ -0,0 +1,20 @@
+/*
+*Esse documento faz parte de uma aplicação desenvolvida na matéria de APSOO-2018/2 na FACOM, 
+*disciplina lecionada pelo Profª Edilson Palma
+*Seu uso é permitido para fins acadêmicos, todavia mantendo a referência de autoria.
+*@author Alexya M. Viana Domingues - Acadêmica de Engenharia de Software
+*@author Bruna Luzia A. Rodrigues - Acadêmica de Engenharia de Software
+*@author Hanna Karoline T. Palacios - Acadêmica de Engenharia de Software
+*@author Gabriel Fernando Texeira - Acadêmica de Engenharia de Software
+-->Classe Principal que tem como tratar a conexão com o banco de dados, assim como as buscas,
+    insersões de dados e etc.
+*/
+package principal;
+/*
+ * @author Bruna Luzia A. Rodrigues
+ */
+public class BrinqueFeliz {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
