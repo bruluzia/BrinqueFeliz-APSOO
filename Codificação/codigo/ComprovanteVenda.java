7a2fe4e0706679a2e4d65b8b22bc431ed34b04a3
Bruna Luzia A. Rodrigues <45202866+bruluzia@users.noreply.github.com>
2018-12-02 14:43:39 -0300

    Adicionamos comentários

--- Codificação/codigo/ComprovanteVenda.java
+++ Codificação/codigo/ComprovanteVenda.java
@@ -1,4 +1,17 @@
+/*
+*Esse documento faz parte de uma aplicação desenvolvida na matéria de APSOO-2018/2 na FACOM, 
+*disciplina lecionada pelo Profª Edilson Palma
+*Seu uso é permitido para fins acadêmicos, todavia mantendo a referência de autoria.
+*@author Alexya M. Viana Domingues - Acadêmica de Engenharia de Software
+*@author Bruna Luzia A. Rodrigues - Acadêmica de Engenharia de Software
+*@author Hanna Karoline T. Palacios - Acadêmica de Engenharia de Software
+*@author Gabriel Fernando Texeira - Acadêmica de Engenharia de Software
+-->Classe ComprovanteVebda que tem como objetivo tratar a geração de comprovantes da venda
+*/
 package codigo;
+/*
+*@author Gabriel Fernando Teixeira
+*/
 
 public class ComprovanteVenda {
 
