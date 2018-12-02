018b2f2dc071c8ca8cdafa2e7be5b1ea68bdb725
Bruna Luzia A. Rodrigues <45202866+bruluzia@users.noreply.github.com>
2018-12-02 14:49:28 -0300

    Adicionamos comentários

--- Codificação/codigo/Venda.java
+++ Codificação/codigo/Venda.java
@@ -1,5 +1,17 @@
+/*
+*Esse documento faz parte de uma aplicação desenvolvida na matéria de APSOO-2018/2 na FACOM, 
+*disciplina lecionada pelo Profª Edilson Palma
+*Seu uso é permitido para fins acadêmicos, todavia mantendo a referência de autoria.
+*@author Alexya M. Viana Domingues - Acadêmica de Engenharia de Software
+*@author Bruna Luzia A. Rodrigues - Acadêmica de Engenharia de Software
+*@author Hanna Karoline T. Palacios - Acadêmica de Engenharia de Software
+*@author Gabriel Fernando Texeira - Acadêmica de Engenharia de Software
+-->Classe Venda que tem como objetivo tratar o gerenciamento de dados da venda
+*/
 package codigo;
-
+/*
+*@author 
+*/
 import java.sql.Time;
 import java.util.Date;
 
