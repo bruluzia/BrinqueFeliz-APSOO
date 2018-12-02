4178b20c150e38547805e5390646a98d98c9ceb6
Bruna Luzia A. Rodrigues <45202866+bruluzia@users.noreply.github.com>
2018-12-02 14:47:51 -0300

    Adicionamos comentários

--- Codificação/codigo/Pagamento.java
+++ Codificação/codigo/Pagamento.java
@@ -1,5 +1,17 @@
+/*
+*Esse documento faz parte de uma aplicação desenvolvida na matéria de APSOO-2018/2 na FACOM, 
+*disciplina lecionada pelo Profª Edilson Palma
+*Seu uso é permitido para fins acadêmicos, todavia mantendo a referência de autoria.
+*@author Alexya M. Viana Domingues - Acadêmica de Engenharia de Software
+*@author Bruna Luzia A. Rodrigues - Acadêmica de Engenharia de Software
+*@author Hanna Karoline T. Palacios - Acadêmica de Engenharia de Software
+*@author Gabriel Fernando Texeira - Acadêmica de Engenharia de Software
+-->Classe Pagamento que tem como objetivo principal gerenciar o pagamento.
+*/
 package codigo;
-
+/*
+*@author
+*/
 public class Pagamento {
 
     private int tipoPagamento;
